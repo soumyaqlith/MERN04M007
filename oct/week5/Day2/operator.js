6c7b3a64d6babeb0c98208eb846773dc38617b25
Devlopersoumya <soumya23.microsoftgmail.com>
2025-10-28 22:54:01 +0530

    variable

---
+++ oct/week5/Day2/operator.js
@@ -0,0 +1,68 @@
+console.log("today we are going to learn operator")
+
+let a = 2;
+let b = 3;
+// var a;
+console.log(2 + 3)
+console.log(a + 2)
+
+console.log(typeof (a + 2))
+
+// console.log(a-2)
+
+console.log(a * b)
+
+console.log(a / b)
+
+
+console.log(b % a);
+
+console.log(a ** b);
+
+
+// increament  (post ,pre)
+// decrement   (post ,pre)
+
+// console.log(a++)
+// console.log(a)
+
+// console.log(++a)
+
+// console.log(a--)
+// console.log(a)
+
+
+// let c=a;
+// c=c+2;
+// c+=2;
+// console.log(c)
+
+//! comparision operator
+console.log(a <= b)
+console.log(a > b)
+
+
+let num1 = 2;
+let num2 = "1";
+// console.log(num1===num2)
+
+
+console.log(num1 !== num2);
+
+console.log((3 > 2) && (4 > 5))
+console.log((3 > 2) || (4 > 5))
+console.log(!true)
+console.log(!false)
+
+console.log(((num2++ > 4) && (num1 === num2)) || (num1-- != num2))
+
+
+// let n="1";
+// console.log(n)
+
+let str1="test";
+console.log(1+3-1+        +str1+4+ +"5-")
+
+
+
+
